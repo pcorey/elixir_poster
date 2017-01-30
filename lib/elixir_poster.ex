460f189a841b8dd05de401b806d4e93ed8e2f7d6
Pete Corey <petecorey@gmail.com>
2017-01-30 14:47:58 -0500

    Lots of fixes and changes

--- lib/elixir_poster.ex
+++ lib/elixir_poster.ex
@@ -18,14 +18,17 @@ defmodule ElixirPoster do
   @doc """
   iex> ElixirPoster.join_code("if (true) {\\n    bang;\\n}")
   "if (true) { bang; }"
+  iex> ElixirPoster.join_code("{\\n    '    ';\\n}")
+  "{ '    '; }"
   iex> ElixirPoster.join_code("  a \\n bb\\n \\tc\\n    d  ")
-  " a bb c d "
+  "a bb c d"
   """
   def join_code(code) do
     Logger.debug("Joining code...")
     code
-    |> String.replace(~r/\n/, " ")
-    |> String.replace(~r/\s+/, " ")
+    |> String.trim
+    |> String.replace(~r/\s*\n+\s*/, " ")
+    |> String.replace(~r/\s/," ")
   end
 
   def load_code(data = %PosterData{code_path: code_path}) do
@@ -57,9 +60,8 @@ defmodule ElixirPoster do
 
   def get_pixel_reducer(code, width, ratio, y) do
     fn ({pixel, x}, pixels) ->
-      color = pixel |> to_hex
       character = Enum.at(code, y * width + x)
-      merge_pixel_into_row(color, character, x * ratio, y, pixels)
+      merge_pixel_into_row(to_hex(pixel), character, x * ratio, y, pixels)
     end
   end
 
@@ -74,35 +76,32 @@ defmodule ElixirPoster do
   end
 
   def construct_text_elements(data = %PosterData{code: code,
-                                       ratio: ratio,
-                                       image: %{width: width, pixels: pixels}}) do
+                                                 ratio: ratio,
+                                                 image: %{width: width, pixels: pixels}}) do
     Logger.debug("Constructing text elements...")
-
     text_elements = pixels
     |> Enum.with_index
     |> Enum.reduce([], get_row_reducer(code, width, ratio))
     |> List.flatten
-
     %{data | text_elements: text_elements}
   end
 
   def construct_svg(data = %PosterData{text_elements: text_elements,
-                             ratio: ratio,
-                             final_width: final_width,
-                             final_height: final_height,
-                             image: %{width: width, height: height}}) do
+                                       ratio: ratio,
+                                       final_width: final_width,
+                                       final_height: final_height,
+                                       image: %{width: width, height: height}}) do
     Logger.debug("Constructing svg with #{length text_elements} text elements...")
-    svg = {
-      :svg,
-      %{
-        viewBox: "0 0 #{width*ratio} #{height}",
-        xmlns: "http://www.w3.org/2000/svg",
-        style: "font-family: 'Source Code Pro'; font-size: 1; font-weight: 900;",
-        width: final_width,
-        height: final_height
-      },
-      text_elements
-    }
+    svg = {:svg,
+           %{
+             viewBox: "0 0 #{width*ratio} #{height}",
+             xmlns: "http://www.w3.org/2000/svg",
+             style: "font-family: 'Source Code Pro'; font-size: 1; font-weight: 900;",
+             width: final_width,
+             height: final_height,
+             "xml:space": "preserve"
+           },
+           text_elements}
     |> XmlBuilder.generate
     %{data | svg: svg}
   end
@@ -128,11 +127,8 @@ defmodule ElixirPoster do
     |> construct_text_elements
     |> construct_svg
     |> save_svg
-    Logger.info("Ok!")
   end
 
-  # def go, do: go(1228/2014, 6000, 9000, "./full_code", "./admithub.png", "out.svg")
-  # def go, do: go(1228/2014, 5400, 7200, "./full_code", "./admithub.png", "out.svg")
-  def go, do: go(1228/2014, 3150, 4050, "./full_code", "./admithub.png", "out.svg")
+  def go, do: go(0.6, 4, 4, "./code", "./image.png", "out.svg")
 
 end
